558749255e90ae4c4dbc74c1e8b4225096026ab8
Flippie <flippievandyk@gmail.com>
2020-12-05 09:46:20 +0200

    Nave solution for Day2 pt 2 Need beetter data ingestion thatn editing with multi-line cursor

--- AdventCs/Advent.cs
+++ AdventCs/Advent.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.ComponentModel.Design;
+using System.Linq;
+using System.Security.Authentication.ExtendedProtection;
 
 namespace AdventCs
 {
@@ -7,8 +9,11 @@ namespace AdventCs
 	{
 		private static int Main(string[] args)
 		{
-			Console.WriteLine(AdventDayOne.ExpenseSumThenProduct(AdventDayOne.list));
-			Console.WriteLine(AdventDayOne.ExpenseSumThenProduct3(AdventDayOne.list));
+			// Console.WriteLine($"Advent day 1 pt1 {AdventDayOne.ExpenseSumThenProduct(AdventDayOne.list)}");
+			// Console.WriteLine($"Advent day 1 pt2 {AdventDayOne.ExpenseSumThenProduct3(AdventDayOne.list)}");
+
+			
+			Console.WriteLine($"Advent day 2 pt1 {AdventDay2.CheckPasswordList(AdventDay2.input.ToList())}");
 
 			return 0;
 		}
